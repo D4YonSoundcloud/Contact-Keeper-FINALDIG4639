769bbf7a0cbff6faa70533bf2ccbcccb7c303261
d4yonsouncloude <mattd4y@gmail.com>
2020-04-08 18:33:52 -0600

    mongodbConnect,userModelMadeWithValidation8:32pm

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const mongoDB = require("mongoose");
+const connectDB = require("./config/db");
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API'}));
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the ContactKeeper API" })
+);
 
 //Define routes
 
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
